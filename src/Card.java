60712a08c578756274e834477b963eaf2910aef4
Grzegorz Ruchniewicz <gregor.redar@gmail.com>
2018-06-09 14:08:35 +0200

    First commit

---
+++ src/Card.java
@@ -0,0 +1,20 @@
+public class Card {
+    private Suits suits;
+    private Ranks ranks;
+
+    public Suits getSuits() {
+        return suits;
+    }
+
+    public void setSuits(Suits suits) {
+        this.suits = suits;
+    }
+
+    public Ranks getRanks() {
+        return ranks;
+    }
+
+    public void setRanks(Ranks ranks) {
+        this.ranks = ranks;
+    }
+}
